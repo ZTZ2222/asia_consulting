45e2ba59c4cb07ad162e027e512c6579f9ed018c
ZTZ <tzamirov@gmail.com>
2024-08-22 18:57:52 +0600

    make blog card open modal on click

--- src/components/shared/blog-card.tsx
+++ src/components/shared/blog-card.tsx
@@ -1,10 +1,19 @@
 "use client"
 
 import Image from "next/image"
-import Link from "next/link"
 import { ArrowUpRight } from "lucide-react"
 import { useLocale, useTranslations } from "next-intl"
 import { cn } from "@/lib/utils"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { ScrollArea } from "@/components/ui/scroll-area"
 import type { NormalizedArticleRead } from "@/types/blog.schema"
 
 type BlogCardProps = NormalizedArticleRead & {
@@ -25,46 +34,76 @@ export default function BlogCard({
   const locale = useLocale()
   const t = useTranslations()
   return (
-    <Link
-      href={`/blog/${uid}`}
-      className={cn(
-        "shrink-0 space-y-8 lg:max-w-[317px] xl:max-w-[402px]",
-        size === "sm" ? "max-w-[300px]" : "",
-        className,
-      )}
-    >
-      {/* Image */}
-      <div className={cn("relative h-[240px] overflow-hidden rounded-lg")}>
-        <Image
-          src={image}
-          alt={`Blog #${uid} image`}
-          fill
-          className="object-cover"
-          sizes="(max-width: 768px) 100vw, 50vw"
-        />
-      </div>
-
-      {/* Info */}
-      <div className="flex flex-col gap-5">
-        <span className="text-sm font-medium leading-[18.2px] text-rose-750">
-          {updatedAt?.toLocaleDateString(locale) ||
-            createdAt.toLocaleDateString(locale)}
-        </span>
-        <div className="flex gap-4">
-          <h4 className="line-clamp-2 text-lg font-bold leading-[21.6px] text-black">
-            {title}
-          </h4>
-          <ArrowUpRight className="size-6 shrink-0" />
+    <Dialog>
+      <DialogTrigger
+        className={cn(
+          "shrink-0 space-y-8 text-start lg:max-w-[317px] xl:max-w-[402px]",
+          size === "sm" ? "max-w-[300px]" : "",
+          className,
+        )}
+      >
+        {/* Image */}
+        <div className={cn("relative h-[240px] overflow-hidden rounded-lg")}>
+          <Image
+            src={image}
+            alt={`Blog #${uid} image`}
+            fill
+            className="object-cover"
+            sizes="(max-width: 768px) 100vw, 50vw"
+          />
         </div>
-        <div
-          className="line-clamp-3 text-gray-650"
-          dangerouslySetInnerHTML={{ __html: content }}
-        />
 
-        <div className="text-end font-semibold leading-5 text-gray-650">
-          {t("Components.Button.read-more")}
+        {/* Info */}
+        <div className="flex flex-col gap-5">
+          <span className="text-sm font-medium leading-[18.2px] text-rose-750">
+            {updatedAt?.toLocaleDateString(locale) ||
+              createdAt.toLocaleDateString(locale)}
+          </span>
+          <div className="flex gap-4">
+            <h4 className="line-clamp-2 text-lg font-bold leading-[21.6px] text-black">
+              {title}
+            </h4>
+            <ArrowUpRight className="size-6 shrink-0" />
+          </div>
+          <div
+            className="line-clamp-3 text-gray-650"
+            dangerouslySetInnerHTML={{ __html: content }}
+          />
+
+          <div className="text-end font-semibold leading-5 text-gray-650">
+            {t("Components.Button.read-more")}
+          </div>
         </div>
-      </div>
-    </Link>
+      </DialogTrigger>
+      <DialogContent className="rounded-3xl px-3">
+        <ScrollArea className="h-[640px]">
+          <div className="space-y-8">
+            <DialogHeader className="space-y-2 text-start">
+              <DialogTitle className="text-2xl font-semibold text-blue-950">
+                {title}
+              </DialogTitle>
+              <DialogDescription className="text-red-550 text-sm font-semibold">
+                {updatedAt?.toLocaleDateString(locale) ||
+                  createdAt.toLocaleDateString(locale)}
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter className="flex-col gap-5 sm:flex-col">
+              <div className="relative h-[268px] min-w-[354px] overflow-hidden rounded-2xl">
+                <Image
+                  src={image}
+                  alt={`Blog #${uid} image`}
+                  fill
+                  className="object-cover"
+                />
+              </div>
+              <div
+                className="text-gray-650"
+                dangerouslySetInnerHTML={{ __html: content }}
+              />
+            </DialogFooter>
+          </div>
+        </ScrollArea>
+      </DialogContent>
+    </Dialog>
   )
 }
